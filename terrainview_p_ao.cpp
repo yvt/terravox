52c945eedbc178b1c6967e4c3b695454e9088b2b
yvt <i@yvt.jp>
2015-03-01 01:32:34 +0900

    Fixed #3 (3)

--- terrainview_p_ao.cpp
+++ terrainview_p_ao.cpp
@@ -392,9 +392,9 @@ void TerrainView::TerrainViewPrivate::applyAmbientOcclusion()
                 col = _mm_packus_epi16(colLo, colHi);
 
                 // keep background color original
-                col = _mm_and_si128(isForeground, col);
-                originalColor = _mm_andnot_ps(isForeground, originalColor);
-                col = _mm_or_ps(col, originalColor);
+                col = _mm_and_si128(_mm_castps_si128(isForeground), col);
+                originalColor = _mm_andnot_si128(_mm_castps_si128(isForeground), originalColor);
+                col = _mm_or_si128(col, originalColor);
 
                 _mm_storeu_si128(reinterpret_cast<__m128i*>(lineColorOutput), col);
 
