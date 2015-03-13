a401b32dabb42c7581f7a96489f07056762f77f0
yvt <i@yvt.jp>
2015-03-14 00:00:17 +0900

    Translation file is located correctly on Windows

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <QLibraryInfo>
 #include "cpu.h"
 #include <QMessageBox>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -23,9 +24,16 @@ int main(int argc, char *argv[])
     darkPalette.setColor(QPalette::ToolTipBase, QColor(210, 210, 210));
     darkPalette.setColor(QPalette::ToolTipText, QColor(210, 210, 210));
     darkPalette.setColor(QPalette::Text, QColor(210, 210, 210));
+    darkPalette.setColor(QPalette::Disabled, QPalette::Text, QColor(80, 80, 80));
     darkPalette.setColor(QPalette::Button, QColor(53,53,53));
     darkPalette.setColor(QPalette::ButtonText, QColor(210, 210, 210));
     darkPalette.setColor(QPalette::BrightText, Qt::red);
+
+    darkPalette.setColor(QPalette::Disabled, QPalette::Dark, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::Disabled, QPalette::Light, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::Disabled, QPalette::Midlight, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::Disabled, QPalette::Mid, QColor(53, 53, 53));
+
     darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
 
     darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
@@ -33,7 +41,8 @@ int main(int argc, char *argv[])
 
     a.setPalette(darkPalette);
 
-    a.setStyleSheet("*{font-size: 11px;} QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+    a.setStyleSheet("*{font-size: 12px;} "
+                    "QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
     QTranslator qtTranslator;
     qtTranslator.load("qt_" + QLocale::system().name(),
@@ -42,10 +51,18 @@ int main(int argc, char *argv[])
 
     QTranslator myappTranslator;
     QString dir;
-#ifdef Q_OS_DARWIN
+#if defined(Q_OS_DARWIN)
     dir = QApplication::applicationDirPath() + "/../Resources";
+#elif defined(Q_OS_WIN)
+    dir = QApplication::applicationDirPath() + "/Locales";
 #endif
-    myappTranslator.load(QLocale::system(), "terravox_", QString(), dir);
+    qDebug() << "Translation directory: " << dir;
+    qDebug() << "Locale: " << QLocale::system().name();
+    if (!myappTranslator.load("terravox_" + QLocale::system().name(), dir))
+    {
+        qDebug() << "warning: translation for " << QLocale::system() << " not loaded.";
+    }
+
     a.installTranslator(&myappTranslator);
 
     if (!CpuId::supports(CpuId::Feature::Sse, CpuId::Feature::Sse2)) {
